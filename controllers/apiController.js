a24fdad6dadd508c8bc79a45b7a74007dfdb753d
Aaron Gaither <AaronGaither@coresystems.local>
2017-07-30 22:23:28 -0700

    initial commit

---
+++ controllers/apiController.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = app => {
+
+  app.get("/api/stylists", (req, res) => res.json());
+
+};
--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 module.exports = app => {
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+  app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+
+  app.get("/results", (req, res) => res.sendFile(path.join(__dirname, "../public/results.html")));
 
 };
