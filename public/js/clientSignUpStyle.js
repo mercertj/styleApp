9417ce7786167bd1b702188960d16e40ae26bdff
leenbean16 <shamoonlina@gmail.com>
2017-08-05 13:25:59 -0700

    profile

---
+++ public/js/clientSignUpStyle.js
@@ -0,0 +1,17 @@
+var color = $('.color').hide();
+
+$('#modal-btn-one').on('click', function() {
+    $('#modal').fadeIn("fast", function() {});
+});
+
+$('.delete').on('click', function() {
+    $('.modal').fadeOut("fast", function() {});
+});
+
+$('#modal-btn-two').on('click', function() {
+    $('#modal-two').fadeIn("fast", function() {});
+});
+
+$('.delete').on('click', function() {
+    $('#modal-two').fadeOut("fast", function() {});
+});
