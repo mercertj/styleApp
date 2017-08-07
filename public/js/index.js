481893d7d9f87151fe8999a3b4f7888a0a45e138
Aaron Gaither <AaronGaither@coresystems.local>
2017-08-07 00:15:33 -0700

    update with uri redirect

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,18 @@
 $(document).ready(function() {
-    console.log("file loaded");
-    // Adding an event listener to the form to create a new object
     $(document).on("click", "#search-all", searchAll);
 
     function serviceSearch() {
-
-        console.log("button clicked");
-        return $('#service-search').val()
+        return $('#service-search').val().replace(' ', '+');
     }
 
     function distanceSearch() {
-
-        console.log("button-clicked");
         return $('#distance').val()
     }
-
-    function searchStylist(stylistData) {
-        $.post("/list", stylistData)
-    };
 
     function searchAll() { 
     	var svcSearch = serviceSearch();
     	var distSearch = distanceSearch();
-    	searchStylist({
-    		"specialty": svcSearch, 
-    		"distance-search": distSearch
-    	})
-        console.log("search clicked");
-        console.log(serviceSearch(), distanceSearch());
-
+        window.location.href = "../list/?service=" + svcSearch +"&range=" + distSearch+"/"
     }
 
 });
