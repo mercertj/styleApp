0399ace1cb299b439995111856dde0d86ec1be03
Thomas Mercer <tmercer74@gmail.com>
2017-08-12 00:12:27 -0700

    updated info in apiTest.js

--- controllers/apiTest.js
+++ controllers/apiTest.js
@@ -3,10 +3,10 @@ const router = require("express").Router();
 
 const _stylistData = [
 	{
-		first_name: 'Lina',
-		last_name: 'Shamoon',
-		email: 'lina@ucsdext.org',
-		bio: "This is Lina's bio.",
+		first_name: 'Linda',
+		last_name: 'Smith',
+		email: 'linda@ucsdext.org',
+		bio: "I have been a freelance hairstylist for 1 year, and I consider myself lucky to have such a fun and creative job! It is very important to me that my clients feel confident and comfortable throughout their service; therefore a thorough consultation is always part of the appointment. I can customize your cut, color and style to suit your lifestyle and your aesthetic. I want your style to compliment you and work for you. I love that I can provide a service that is both artistic and practical.",
 		experience: 1,
 		phone_number: '619-123-4567',
 		cut: true,
@@ -26,10 +26,10 @@ const _stylistData = [
 		address_long: -117.203928
 	},
 	{
-		first_name: 'Tom',
-		last_name: 'Mercer',
-		email: 'tom@ucsdext.org',
-		bio: "This is Tom's bio.",
+		first_name: 'Peter',
+		last_name: 'Griffin',
+		email: 'peter@ucsdext.org',
+		bio: "I specialize in custom cuts for women, men and children; personalized color, including highlighting and lowlighting; and formal hair designs. I am fully mobile, and bring the salon experience directly to you",
 		experience: 2,
 		phone_number: '619-321-4567',
 		cut: true,
@@ -49,10 +49,10 @@ const _stylistData = [
 		address_long: -117.184973
 	},
 	{
-		first_name: 'Fabio',
+		first_name: 'Danny',
 		last_name: 'Aiello',
-		email: 'fabio@ucsdext.org',
-		bio: "This is Fabio's bio.",
+		email: 'danny@ucsdext.org',
+		bio: "This is Danny's bio.",
 		experience: 3,
 		phone_number: '619-321-7654',
 		cut: true,
@@ -70,6 +70,29 @@ const _stylistData = [
 		zipcode: '92111',
 		address_lat: 32.860259,
 		address_long: -117.187881
+	},
+	{
+		first_name: 'Shannon',
+		last_name: 'Stein',
+		email: 'shanstylist2000@ucsdext.org',
+		bio: "My name is Shannon and am a super passionate curly girl who loves working with all types of hair texture. You are a curly girl reading my bio… are you ready for a new wild look? Subtle changes? Don’t hesitate to book your appointment with me and become a part of my family. I would love to meet you soon! ",
+		experience: 3,
+		phone_number: '619-321-7654',
+		cut: true,
+		blow_dry: false,
+		color: false,
+		highlights: false,
+		lowlights: false,
+		ombre: true,
+		balayage: false,
+		hair_do: true,
+		travel_range: 30,
+		street_address: '5987 El Cajon Blvd',
+		city: 'San Diego',
+		state: 'CA',
+		zipcode: '92115',
+		address_lat: 32.760458,
+		address_long: -117.068455
 	}
 ]
 
